f555fff6b2556f4ea3fb9e982c9ea6023e0e9269
kelvinzer0 <kelvinandriancom@gmail.com>
2025-06-30 13:44:25 +0700

    Fix installer script to use sudo tee for writing to systemd directory

--- scripts/make.sh
+++ scripts/make.sh
@@ -5,6 +5,6 @@ _path=`pwd`
 _dir=`dirname $_path`
 
 
-sed "s:{APP_PATH}:${_dir}:g" $_dir/scripts/init.d/imail.service.tpl > /etc/systemd/system/imail.service
+sed "s:{APP_PATH}:${_dir}:g" $_dir/scripts/init.d/imail.service.tpl | sudo tee /etc/systemd/system/imail.service > /dev/null
 
 echo `dirname $_path`
