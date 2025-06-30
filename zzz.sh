162317ac1137f6a930a9a03cc9246757eade1b5c
kelvinzer0 <kelvinandriancom@gmail.com>
2025-06-30 14:48:16 +0700

    Remove pick.sh and enable CGO for SQLite

--- zzz.sh
+++ zzz.sh
@@ -6,6 +6,8 @@ go env -w GOSUMDB=off
 export GIT_COMMIT=$(git rev-parse HEAD)
 export BUILD_TIME=$(date -u '+%Y-%m-%d %I:%M:%S %Z')
 
+export CGO_ENABLED=1
+
 # zzz run -ldflags "-w -s -X \"github.com/kelvinzer0/imail/internal/conf.BuildCommit=$(git rev-parse HEAD)\" -X \"github.com/kelvinzer0/imail/internal/conf.BuildTime=$(date -u '+%Y-%m-%d %I:%M:%S %Z')\""
 
 echo zzz run -ldflags "-X \"github.com/kelvinzer0/imail/internal/conf.BuildTime=${BUILD_TIME}\" -X \"github.com/kelvinzer0/imail/internal/conf.BuildCommit=${GIT_COMMIT}\""
