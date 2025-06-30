ad20b0ffc6141e5b85ac14b6ba7689909abe3749
kelvinzer0 <kelvinandriancom@gmail.com>
2025-06-30 13:33:59 +0700

    Fix installer issues

--- scripts/make.sh
+++ scripts/make.sh
@@ -4,15 +4,7 @@ _os=`uname`
 _path=`pwd`
 _dir=`dirname $_path`
 
-sed "s:{APP_PATH}:${_dir}:g" $_dir/scripts/init.d/imail.tpl > $_dir/scripts/init.d/imail
-chmod +x $_dir/scripts/init.d/imail
 
-
-sed "s:{APP_PATH}:${_dir}:g" $_dir/scripts/init.d/imail.service.tpl > $_dir/scripts/init.d/imail.service
-
-if [ -d /etc/init.d ];then
-	cp $_dir/scripts/init.d/imail /etc/init.d/imail
-	chmod +x /etc/init.d/imail
-fi
+sed "s:{APP_PATH}:${_dir}:g" $_dir/scripts/init.d/imail.service.tpl > /etc/systemd/system/imail.service
 
 echo `dirname $_path`
